5a7526ef7b782e47867fd0efbd92b12eb548be7f
Roman Siva <RS212418@reigate.ac.uk>
2021-11-23 16:12:48 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Would you like to purchase a raffle ticket or check for an existing purchase? Purchase or Check");
+        String userInput = input.next();
+        
+
+    }
+}
